edae5c1ca5b5f34d636e2a972c20e07d4eed0e59
Antonio Lopes <antoniolopesg@protonmail.com>
2021-04-15 18:43:58 -0300

    fix: statusbar over home component

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components/native'
 
-export const Container = styled.SafeAreaView`
+export const Container = styled.View`
   background-color: #260c1a;
   flex: 1;
   padding: 16px;
