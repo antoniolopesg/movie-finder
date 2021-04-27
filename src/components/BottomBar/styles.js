4775e2e3a6fe143b1d7df24bbcc254586928ee37
Antonio Lopes <antoniolopesg@protonmail.com>
2021-04-27 18:16:33 -0300

    fix: ensure bottom tabs render over controls on iphone

--- src/components/BottomBar/styles.js
+++ src/components/BottomBar/styles.js
@@ -1,12 +1,12 @@
 import {RectButton} from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
+import {getBottomSpace} from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
-  height: 70px;
-
   width: 100%;
-
   flex-direction: row;
+  background-color: #fff;
+  padding-bottom: ${getBottomSpace()}px;
 `;
 export const RouteButton = styled(RectButton)`
   flex: 1;
@@ -14,6 +14,8 @@ export const RouteButton = styled(RectButton)`
 
   justify-content: center;
   align-items: center;
+
+  padding: 10px;
 `;
 
 export const RouteText = styled.Text`
